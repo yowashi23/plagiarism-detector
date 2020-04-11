e5d780e66569f2262bf68a5fd65b4e104ff78a4f
yowashi23 <shin.yongwhan23@gmail.com>
2020-04-10 20:37:01 -0500

    dataset path changed

--- detector.py
+++ detector.py
@@ -1,4 +1,5 @@
 # import csv
+# why doesn't it change
 import pandas as pd
 data = pd.read_csv("../user-ct-test-collection-01.txt", sep="\t")
 querylist = data.Query.dropna()[data.Query.dropna() != "-"]
