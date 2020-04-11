3971128bf76669ce8b1eb2aff4b41b8bc135c61c
yowashi23 <shin.yongwhan23@gmail.com>
2020-04-10 20:31:59 -0500

    dataset path changed

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 # import csv
 import pandas as pd
-data = pd.read_csv("./user-ct-test-collection-01.txt", sep="\t")
+data = pd.read_csv("../user-ct-test-collection-01.txt", sep="\t")
 querylist = data.Query.dropna()[data.Query.dropna() != "-"]
 print(querylist.head())
 
