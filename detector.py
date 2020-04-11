a28e2b027547588d3fec072ca0636be1b6803156
yowashi23 <shin.yongwhan23@gmail.com>
2020-04-10 21:59:40 -0500

    simple minhash generator implemented

--- detector.py
+++ detector.py
@@ -1,7 +1,29 @@
-# import csv
-# why doesn't it change
-import pandas as pd
-data = pd.read_csv("../user-ct-test-collection-01.txt", sep="\t")
-querylist = data.Query.dropna()[data.Query.dropna() != "-"]
-print(querylist.head())
+# Implement simple MinHash generator
+from sklearn.utils import murmurhash3_32
+import random
+random.seed(24)
 
+
+def hashfunc_gen():
+    rand_seed = random.randint(0, 2^24)
+    return lambda x: murmurhash3_32(x, seed=rand_seed)
+
+
+def hashcodes_gen(string, m):
+    hashcodes = []
+    for i in range(m):
+        hashcodes.append(hashfunc_gen()(m))
+    return hashcodes
+
+print(hashcodes_gen("aids", 100))
+
+
+
+
+# Import dataset
+# import csv
+# import pandas as pd
+# data = pd.read_csv("../user-ct-test-collection-01.txt", sep="\t")
+# urllist = data.ClickURL.dropna().unique()
+# print(urllist)
+# print(2**24)
