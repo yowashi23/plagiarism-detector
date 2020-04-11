3e7a75935df714113a089c42c7fc44219259bc1b
yowashi23 <shin.yongwhan23@gmail.com>
2020-04-10 19:48:06 -0500

    data read in

--- detector.py
+++ detector.py
@@ -1,1 +1,6 @@
-print("Hi")
+# import csv
+import pandas as pd
+data = pd.read_csv("./user-ct-test-collection-01.txt", sep="\t")
+querylist = data.Query.dropna()[data.Query.dropna() != "-"]
+print(querylist.head())
+
